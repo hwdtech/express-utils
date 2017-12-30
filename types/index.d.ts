43518f0492dc27a53920d5e4e1bfb3c0ffa8ebfe
Maxim Malov <mmalov@hwdtech.ru>
2017-12-30 16:21:28 +0600

    Transfer repo and fix typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,52 +1,50 @@
-declare module 'express-utils' {
-  import { Writable } from 'stream';
+import { Writable } from 'stream';
 
-  export interface Config {
-    get(prop: string): any
-  }
+export interface Config {
+  get(prop: string): any
+}
 
-  export interface Logger {
-    trace(...params: any[]): void;
-    debug(...params: any[]): void;
-    info(...params: any[]): void;
-    warn(...params: any[]): void;
-    error(...params: any[]): void;
-    fatal(...params: any[]): void;
-    child(opts?: Object): Logger;
-    reopenFileStreams(): void;
-  }
+export interface Logger {
+  trace(...params: any[]): void;
+  debug(...params: any[]): void;
+  info(...params: any[]): void;
+  warn(...params: any[]): void;
+  error(...params: any[]): void;
+  fatal(...params: any[]): void;
+  child(opts?: Object): Logger;
+  reopenFileStreams(): void;
+}
 
-  export class ConfigBuilder {
-    fromEnv(): ConfigBuilder;
-    fromFile(file: string): ConfigBuilder;
-    fromArgs(): ConfigBuilder;
-    setRequiredFields(keys: string[]): ConfigBuilder;
-    build(): Config
-  }
+export class ConfigBuilder {
+  fromEnv(): ConfigBuilder;
+  fromFile(file: string): ConfigBuilder;
+  fromArgs(): ConfigBuilder;
+  setRequiredFields(keys: string[]): ConfigBuilder;
+  build(): Config
+}
 
-  export type StreamType = 'raw' | 'file' | 'pretty';
-  export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error';
+export type StreamType = 'raw' | 'file' | 'pretty';
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error';
 
-  export interface StreamConfiguration {
-    type: StreamType,
-    level: LogLevel,
-    stream?: Writable,
-    path?: string
-  }
+export interface StreamConfiguration {
+  type: StreamType,
+  level: LogLevel,
+  stream?: Writable,
+  path?: string
+}
 
-  export class LoggerBuilder {
-    name(n: string): LoggerBuilder;
-    addStream(config: StreamConfiguration): LoggerBuilder;
-    addStreams(configs: StreamConfiguration[]): LoggerBuilder;
-    addSerializer(name: string, serializer: Function): LoggerBuilder;
-    build(): Logger;
-  }
+export class LoggerBuilder {
+  name(n: string): LoggerBuilder;
+  addStream(config: StreamConfiguration): LoggerBuilder;
+  addStreams(configs: StreamConfiguration[]): LoggerBuilder;
+  addSerializer(name: string, serializer: Function): LoggerBuilder;
+  build(): Logger;
+}
 
-  export class LoggerFactory {
-    constructor(logger: Logger);
-    getLogger(name: string): Logger;
-    turnOnNativeLogRotation(signal?: string): void;
+export class LoggerFactory {
+  constructor(logger: Logger);
+  getLogger(name: string): Logger;
+  turnOnNativeLogRotation(signal?: string): void;
 
-    private eachLogger(): void
-  }
+  private eachLogger(): void
 }
